9ac9d9d9995b4055e586d0073af8e9de53cbe009
unknown <ndelsaux@LFR005669.corp.capgemini.com>
2015-08-13 14:15:14 +0200

    Rewrote test to make it more clear ... and easier to send to StackOverflow

--- jcr-test/src/test/java/org/wisdom/jcrom/BundleUnloadIT.java
+++ jcr-test/src/test/java/org/wisdom/jcrom/BundleUnloadIT.java
@@ -19,20 +19,8 @@
  */
 package org.wisdom.jcrom;
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.BundleContext;
-import org.osgi.framework.BundleException;
-import org.osgi.framework.Constants;
-import org.ow2.chameleon.testing.helpers.OSGiHelper;
-import org.wisdom.api.configuration.ApplicationConfiguration;
-import org.wisdom.test.parents.WisdomTest;
+import static org.ops4j.pax.tinybundles.core.TinyBundles.bundle;
 
-import javax.inject.Inject;
-import javax.tools.JavaCompiler;
-import javax.tools.ToolProvider;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -42,173 +30,174 @@ import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.ops4j.pax.tinybundles.core.TinyBundles.bundle;
+import javax.inject.Inject;
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.BundleException;
+import org.osgi.framework.Constants;
+import org.ow2.chameleon.testing.helpers.OSGiHelper;
+import org.wisdom.api.configuration.ApplicationConfiguration;
+import org.wisdom.test.parents.WisdomTest;
 
 /**
- * Test used to check if all references to mapped classes are correctly removed when a bundle is unloaded.
+ * Test used to check if all references to mapped classes are correctly removed
+ * when a bundle is unloaded.
  * <p/>
- * User: Antoine Mischler <antoine@dooapp.com>
- * Date: 19/06/15
- * Time: 15:29
+ * User: Antoine Mischler <antoine@dooapp.com> Date: 19/06/15 Time: 15:29
  */
 public class BundleUnloadIT extends WisdomTest {
+	/**
+	 * Make sure gc is called by invoking it until that dumbl object is gc'ed
+	 */
+	public static void forceGc() {
+		Object obj = new Object();
+		WeakReference ref = new WeakReference<Object>(obj);
+		obj = null;
+		while (ref.get() != null) {
+			System.gc();
+		}
+	}
 
-    @Inject
-    private BundleContext context;
+	@Inject
+	private BundleContext context;
 
-    @Inject
-    private ApplicationConfiguration applicationConfiguration;
+	@Inject
+	private ApplicationConfiguration applicationConfiguration;
 
-    private OSGiHelper osgi;
+	private OSGiHelper osgi;
 
-    private OSGiUtils osGiUtils;
+	private OSGiUtils osGiUtils;
 
-    private List<Bundle> bundles;
+	private List<Bundle> bundles;
 
-    @Before
-    public void setUp() {
-        osgi = new OSGiHelper(context);
-        osGiUtils = new OSGiUtils(osgi);
-        bundles = new ArrayList<>();
-    }
+	/**
+	 * As class is in entity1 package, and that package is declared in the jcrom section of application.conf
+	 * this class will be mapped by jcrom and have an associated JcrRepository
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws BundleException
+	 */
+	@Test
+	public void a_mapped_class_should_unload_correctly() throws IOException, ClassNotFoundException,
+			BundleException {
+		final String CLASS_PATH = "org/wisdom/jcrom/entity1/BundleUnloadITEntity";
+		loadAndUnloadBundleFor(CLASS_PATH);
+	}
 
-    @After
-    public void tearDown() {
-        for (Bundle bundle : bundles) {
-            try {
-                bundle.uninstall();
-            } catch (BundleException e) {
-                // Ignore it
-            }
-        }
-        try {
-            osgi.dispose();
-        } catch (Exception e) {
-            // Ignore it
-        }
-    }
+	/**
+	 * Reference test.
+	 * To make sure class is not mapped by jcrom, we put it in a "unmapped" package
+	 *
+	 * @throws IOException
+	 * @throws ClassNotFoundException
+	 * @throws BundleException
+	 */
+	@Test
+	public void an_unmapped_class_should_be_unloaded_correctly() throws IOException,
+			ClassNotFoundException, BundleException {
+		String CLASS_PATH = "org/wisdom/jcrom/unmapped/BundleUnloadITEntity";
+		loadAndUnloadBundleFor(CLASS_PATH);
+	}
 
-    //@Test
-    public void testUnload() throws IOException, ClassNotFoundException, BundleException {
-        // create an entity to map
-        String source = "package org.wisdom.jcrom.entity1;\n" +
-                "\n" +
-                "import org.jcrom.AbstractJcrEntity;\n" +
-                "import org.jcrom.annotations.JcrNode;\n" +
-                "import org.jcrom.annotations.JcrName;\n" +
-                "import org.jcrom.annotations.JcrPath;\n" +
-                "\n" +
-                "@JcrNode(nodeType = \"test:testentity\")\n" +
-                "public class BundleUnloadITEntity {\n" +
-                "@JcrName\n" +
-                "private String name;\n" +
+	private File createEntityClass(String classPath) throws IOException {
+		File sourceFile = new File("target", classPath+".java");
+		File classFile = new File("target", classPath+".class");
+		final String packageName = classPath.substring(0, classPath.lastIndexOf('/')).replace('/', '.');
+		final String className  = classPath.substring(classPath.lastIndexOf('/')+1);
+		if (!classFile.exists()) {
+			if (!sourceFile.exists()) {
+				sourceFile.getParentFile().mkdirs();
+				// create an entity to map
+				String source = "package "+packageName+";\n" + "\n"
+						+ "import org.jcrom.AbstractJcrEntity;\n"
+						+ "import org.jcrom.annotations.JcrNode;\n"
+						+ "import org.jcrom.annotations.JcrName;\n"
+						+ "import org.jcrom.annotations.JcrPath;\n" + "\n"
+						+ "@JcrNode(nodeType = \"test:testentity\")\n"
+						+ "public class "+className+" {\n"
+						+ "@JcrName\n" + "private String name;\n" +
 
-                "@JcrPath\n" +
-                "private String path;\n" +
-                "}\n";
+						"@JcrPath\n" + "private String path;\n" + "}\n";
 
-        File sourceFile = new File("target", "BundleUnloadITEntity.java");
-        new FileWriter(sourceFile).append(source).close();
+				new FileWriter(sourceFile).append(source).close();
+			}
+			classFile.getParentFile().mkdirs();
+			JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+			compiler.run(null, null, null, sourceFile.getPath());
+		}
+		return classFile;
+	}
 
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        compiler.run(null, null, null, sourceFile.getPath());
+	private void loadAndUnloadBundleFor(String classPath)
+			throws ClassNotFoundException, IOException {
+		String className = classPath.replace('/', '.');
+		File classFile = createEntityClass(classPath);
+		// create a bundle containing this entity
+		Bundle bundle = osgi
+				.installAndStart(
+						"local:/test",
+						bundle().add(
+								classPath+".class",
+								new FileInputStream(classFile))
+								.set(Constants.IMPORT_PACKAGE,
+										"org.jcrom.annotations")
+								.set(Constants.EXPORT_PACKAGE,
+										"org.jcrom.annotations;uses=\"org.jcrom.annotations\";version=\"0.2.0\"")
+								.build());
+		// load that bundle
+		bundles.add(bundle);
+		// and the given class
+		Class clazz = bundle
+				.loadClass(className);
+		// now create a reference queue and reference to make sure that class is unloaded
+		ReferenceQueue referenceQueue = new ReferenceQueue();
+		WeakReference weakReference = new WeakReference(clazz, referenceQueue);
+		// now start deleting things
+		clazz = null;
+		osgi.uninstall(bundle);
+		// uninstalling bundle is asynchronous, so brutally wait for it
+		while (bundle.getState() != Bundle.UNINSTALLED) {
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		bundle = null;
+		forceGc();
+		Assertions.assertThat(weakReference.isEnqueued()).as("weak reference should have been enqueued, but is not").isTrue();
+	}
 
-        // create a bundle containing this entity
-        Bundle bundle = osgi.installAndStart("local:/test",
-                bundle()
-                        .add("org/wisdom/jcrom/entity1/BundleUnloadITEntity.class", new FileInputStream("target/BundleUnloadITEntity.class"))
-                        .set(Constants.IMPORT_PACKAGE, "org.jcrom.annotations")
-                        .set(Constants.EXPORT_PACKAGE, "org.jcrom.annotations;uses=\"org.jcrom.annotations\";version=\"0.2.0\"")
-                        .build());
-        bundles.add(bundle);
-        Class clazz = bundle.loadClass("org.wisdom.jcrom.entity1.BundleUnloadITEntity");
-        ReferenceQueue referenceQueue = new ReferenceQueue();
-        WeakReference weakReference = new WeakReference(clazz, referenceQueue);
-        assertThat(osGiUtils.getCrud(clazz)).isNotNull();
-        bundle.stop();
-        while (bundle.getState() != Bundle.RESOLVED) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        clazz = null;
-        bundle = null;
-        byte[] b = new byte[40000000];
-        b[b.length - 1] = 1;
-        System.gc();
-        Assert.assertTrue(weakReference.isEnqueued());
-    }
+	@Before
+	public void setUp() throws IOException {
+		osgi = new OSGiHelper(context);
+		osGiUtils = new OSGiUtils(osgi);
+		bundles = new ArrayList<>();
+	}
 
-    /**
-     * Reference test.
-     *
-     * @throws IOException
-     * @throws ClassNotFoundException
-     * @throws BundleException
-     */
-    //@Test
-    public void testUnloadUnmapped() throws IOException, ClassNotFoundException, BundleException {
-        // create an entity to map
-        String source = "package org.wisdom.jcrom.unmapped;\n" +
-                "\n" +
-                "import org.jcrom.AbstractJcrEntity;\n" +
-                "import org.jcrom.annotations.JcrNode;\n" +
-                "import org.jcrom.annotations.JcrName;\n" +
-                "import org.jcrom.annotations.JcrPath;\n" +
-                "\n" +
-                "@JcrNode(nodeType = \"test:testentity\")\n" +
-                "public class BundleUnloadITEntity {\n" +
-                "@JcrName\n" +
-                "private String name;\n" +
-
-                "@JcrPath\n" +
-                "private String path;\n" +
-                "}\n";
-
-        File sourceFile = new File("target", "BundleUnloadITEntity.java");
-        new FileWriter(sourceFile).append(source).close();
-
-        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
-        compiler.run(null, null, null, sourceFile.getPath());
-
-        // create a bundle containing this entity
-        Bundle bundle = osgi.installAndStart("local:/test",
-                bundle()
-                        .add("org/wisdom/jcrom/unmapped/BundleUnloadITEntity.class", new FileInputStream("target/BundleUnloadITEntity.class"))
-                        .set(Constants.IMPORT_PACKAGE, "org.jcrom.annotations")
-                        .set(Constants.EXPORT_PACKAGE, "org.jcrom.annotations;uses=\"org.jcrom.annotations\";version=\"0.2.0\"")
-                        .build());
-        bundles.add(bundle);
-        Class clazz = bundle.loadClass("org.wisdom.jcrom.unmapped.BundleUnloadITEntity");
-        ReferenceQueue referenceQueue = new ReferenceQueue();
-        WeakReference weakReference = new WeakReference(clazz, referenceQueue);
-        clazz = null;
-        osgi.uninstall(bundle);
-        while (bundle.getState() != Bundle.UNINSTALLED) {
-            try {
-                Thread.sleep(100);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        bundle = null;
-        byte[] b = new byte[40000000];
-        b[b.length - 1] = 1;
-        forceGc();
-        Assert.assertTrue(weakReference.isEnqueued());
-    }
-
-    public static void forceGc() {
-        Object obj = new Object();
-        WeakReference ref = new WeakReference<Object>(obj);
-        obj = null;
-        while (ref.get() != null) {
-            System.gc();
-        }
-    }
-
+	@After
+	public void tearDown() {
+		for (Bundle bundle : bundles) {
+			try {
+				if(bundle.getState() != Bundle.UNINSTALLED)
+					bundle.uninstall();
+			} catch (BundleException e) {
+				// Ignore it
+			}
+		}
+		try {
+			osgi.dispose();
+		} catch (Exception e) {
+			// Ignore it
+		}
+	}
 
 }
